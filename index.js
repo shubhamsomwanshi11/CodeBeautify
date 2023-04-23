e35b77aa00d48c5c8814aceee647752f3ebe1a90
shubhamsomwanshi11 <shubhamsomwanshi11@gmail.com>
2023-04-23 13:27:55 +0530

    Updated

---
+++ index.js
@@ -0,0 +1,39 @@
+var code = ""
+var work = [
+    {
+        "date": "Wednesday, 19 April 2023",
+        "Link": "",
+        "Description": "Created new tool random New Zealand Address and worked on other tools.",
+        "Time": "4",
+    },
+    {
+        "date": "Thursday, 20 April 2023",
+        "Link": "",
+        "Description": "Working on Random Generators Website.",
+        "Time": "2",
+    },
+    {
+        "date": "Friday, 21 April 2023",
+        "Link": "",
+        "Description": "Working on Random Generators Website.",
+        "Time": "1",
+    },
+    {
+        "date": "Saturday, 22 April 2023",
+        "Link": "",
+        "Description": "Working on Random Generators Website.",
+        "Time": "2",
+    }
+]
+document.addEventListener("DOMContentLoaded", () => {
+    for (let i = 0; i < work.length; i++) {
+        code += `
+                <tr>
+                    <td>${work[i]["date"]}</td>
+                    <td>${work[i]["Link"]}</td>
+                    <td>${work[i]["Description"]}</td>
+                    <td>${work[i]["Time"]} Hours</td>
+                </tr>`
+    }
+    document.getElementById("tbody").innerHTML = code;
+});
