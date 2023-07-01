a94ead94e7958582a6f807643a7dc676d2ef6f5c
shubhamsomwanshi11 <shubhamsomwanshi11@gmail.com>
2023-07-02 00:26:35 +0530

    Updated 2 July

--- index.js
+++ index.js
@@ -4,24 +4,43 @@ var work = [
         "date": "Tuesday, 27 June 2023",
         "Link": "",
         "Description": `<ol class="has-text-left"><li>Created new tool Memorable Password Generator.</li></ol>`,
-        "Time": "5",
+        "Time": "5:00",
     },
     {
         "date": "Wednesday, 28 June 2023",
         "Link": "",
-        "Description": `<ol class="has-text-left"><li>Created new tool Magic School Name Generator.</li>`,
+        "Description": `<ol class="has-text-left"><li>Created new tool Magic School Name Generator.</li></ol>`,
         "Time": "2:30",
+    },
+    {
+        "date": "Thursday, 29 June 2023",
+        "Link": "",
+        "Description": `<ol class="has-text-left"><li>Created new tool Halloween Costume Generator.</li><li>Created new tool Book Title Generator.</li></ol>`,
+        "Time": "5:10",
+    },
+    {
+        "date": "Saturday, 1 July 2023",
+        "Link": "",
+        "Description": `<ol class="has-text-left"><li>Created new tool Disney Character Generator.</li></ol>`,
+        "Time": "4:20",
     }
 ]
 document.addEventListener("DOMContentLoaded", () => {
     for (let i = work.length - 1; i >= 0; i--) {
+        const date = work[i].date;
+        const link = work[i].Link;
+        const description = work[i].Description;
+        const timeParts = work[i].Time.split(':');
+        const hours = timeParts[0];
+        const minutes = timeParts[1];
+
         code += `
-                <tr>
-                    <td>${work[i]["date"]}</td>
-                    <td>${work[i]["Link"]}</td>
-                    <td>${work[i]["Description"]}</td>
-                    <td>${work[i]["Time"]} Hours</td>
-                </tr>`
+          <tr>
+            <td>${date}</td>
+            <td>${link}</td>
+            <td>${description}</td>
+            <td>${hours} Hours ${minutes} Minutes</td>
+          </tr>`;
     }
     document.getElementById("tbody").innerHTML = code;
 });
